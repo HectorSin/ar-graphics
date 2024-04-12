ddeebb546473145f5fe2cfaa09a64c0d5250e72e
kkang15634 <kkang15634@ajou.ac.kr>
2024-04-13 03:02:37 +0900

    new fixes

--- Mini_Proj/hand_detecting.py
+++ Mini_Proj/hand_detecting.py
@@ -5,9 +5,11 @@ import handDetector as htm
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
-#detector = HandDetector(detectionCon=0.8, maxHands=2)
-detector = htm.handDetector(detectionCon=0.75)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+#detector = htm.handDetector(detectionCon=0.75)
 colorR = (255, 0, 255)
+
+# print(dir(detector))
 
 cx, cy, w, h = 100, 100, 200, 200
 
@@ -17,9 +19,46 @@ while True:
         print("Failed to read frame from video capture. Check your camera connection.")
         break
     img = cv2.flip(img, 1)
-    img = detector.findHands(img)
-    # hands, img = detector.findHands(img)  # with draw
+    #img = detector.findHands(img)
+    hands, img = detector.findHands(img)  # with draw
     # hands = detector.findHands(img, draw=False) # without draw
+    # lmList, _ = detector.findPositions(img)
+    #lmList, _ = (detector.findDistance(8, 12, img))
+
+    if hands:
+        # Information for the first hand detected
+        hand1 = hands[0]  # Get the first hand detected
+        lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
+        bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
+        center1 = hand1['center']  # Center coordinates of the first hand
+        handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
+
+        # Count the number of fingers up for the first hand
+        fingers1 = detector.fingersUp(hand1)
+        print(f'H1 = {fingers1.count(1)}', end=" ")  # Print the count of fingers that are up
+
+        # Calculate distance between specific landmarks on the first hand and draw it on the image
+        length, info, img = detector.findDistance(lmList1[8][0:2], lmList1[12][0:2], img, color=(255, 0, 255),
+                                                  scale=10)
+
+        # Check if a second hand is detected
+        if len(hands) == 2:
+            # Information for the second hand
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]
+            bbox2 = hand2["bbox"]
+            center2 = hand2['center']
+            handType2 = hand2["type"]
+
+            # Count the number of fingers up for the second hand
+            fingers2 = detector.fingersUp(hand2)
+            print(f'H2 = {fingers2.count(1)}', end=" ")
+
+            # Calculate distance between the index fingers of both hands and draw it on the image
+            length, info, img = detector.findDistance(lmList1[8][0:2], lmList2[8][0:2], img, color=(255, 0, 0),
+                                                      scale=10)
+
+        print(" ")  # New line for better readability of the printed output
 
     """
     lmList, _ = detector.findPosition(img)
